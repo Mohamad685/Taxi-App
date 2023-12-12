e99f482e90b54f8b9daaa958168f83ed37fc9871
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-13 00:20:14 +0200

    Add Dashboard Container

--- admin/app/src/pages/adminPage/AdminPage.jsx
+++ admin/app/src/pages/adminPage/AdminPage.jsx
@@ -3,21 +3,15 @@ import "./admin.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import RightContainer from "../../components/rightContainer/RightContainer";
 
-
-
 function AdminPage() {
   return (
     <div className="admin-container flex">
-        {/* <Sidebar className="sidebar-container"/> */}
-        <div className="sidebar-section">
-            <Sidebar />
-        </div>
-        <div className="right-section">
+      <div className="sidebar-section">
+        <Sidebar />
+      </div>
+      <div className="right-section">
         <RightContainer />
-        </div>
-
-
-        
+      </div>
     </div>
   );
 }
