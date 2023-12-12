e3b92832cb59d35d82feb3be85e947eed332c0e4
Alisafa60 <alisafa6100@gmail.com>
2023-12-12 16:51:18 +0200

    adjusted register to accept admins that meet a condition

--- backend/taxi-app/app/Http/Controllers/AuthController.php
+++ backend/taxi-app/app/Http/Controllers/AuthController.php
@@ -20,24 +20,37 @@ class AuthController extends Controller
             'email' => 'required|email|unique:users',
             'password' => 'required|string|min:6',
             'type' => 'required|in:admin,driver,passenger',
+            'img_url' => 'sometimes|string',
+            'invitation_code' => 'sometimes|required_if:type,admin|exists:users,invitation_code',
         ]);
-
+    
         $type = DB::table('types')->where('name', $request->input('type'))->first();
-
+    
         if (!$type) {
             return response()->json(['message' => 'Role not found'], 400);
         }
-
+    
+        // Check if the user is registering as an admin
+        if ($request->input('type') === 'admin') {
+            $invitationCode = $request->input('invitation_code');
+    
+            if (!$invitationCode || !User::where('invitation_code', $invitationCode)->exists()) {
+                return response()->json(['error' => 'Invalid or missing invitation code for admin registration'], 403);
+            }
+        }
+    
         $user = User::create([
             'first_name' => $request->input('first_name'),
             'last_name' => $request->input('last_name'),
             'email' => $request->input('email'),
             'password' => Hash::make($request->input('password')),
             'type_id' => $type->id,
+            'img_url' => $request->input('img_url'),
         ]);
-
+    
         return response()->json(['user' => $user, 'message' => 'User registered successfully'], 201);
     }
+        
 
     public function login(Request $request)
     {
