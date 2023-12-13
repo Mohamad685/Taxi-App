206fc0fea830dcac573afd0c3c0abf455067ba8d
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-13 02:23:09 +0200

    Fix Admin Page Issues

--- admin/app/src/components/rightContainer/RightContainer.jsx
+++ admin/app/src/components/rightContainer/RightContainer.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import './rightContainer.css'
+import "./rightContainer.css";
 import Dashboard from "../Dashboard/Dashboard";
-import UsersSection from '../UsersSection/UsersSection'
+import UsersSection from "../UsersSection/UsersSection";
 import ManageDrivers from "../ManageDrivers/ManageDrivers";
 import MessagesSection from "../Messages/MessagesSection";
 
-function RightContainer() {
+function RightContainer({ sectionState }) {
   return (
     <div>
-      <Dashboard />
-      <UsersSection />
-      <ManageDrivers />
-      <MessagesSection />
+      <div className={sectionState.dashboard === 1 ? "" : "hidden"}>
+        <Dashboard />
+      </div>
+      <div className={sectionState.users === 1 ? "" : "hidden"}>
+        <UsersSection />
+      </div>
+      <div className={sectionState.manageDrivers === 1 ? "" : "hidden"}>
+        <ManageDrivers />
+      </div>
+      <div className={sectionState.messages === 1 ? "" : "hidden"}>
+        <MessagesSection />
+      </div>
     </div>
   );
 }
