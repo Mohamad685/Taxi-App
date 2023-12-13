6d11e1eef7daa4e38d53548d7fed275e1aa7a2c5
Alisafa60 <alisafa6100@gmail.com>
2023-12-13 16:04:17 +0200

    updated request and rating controllers

--- backend/app/Http/Controllers/RatingController.php
+++ backend/app/Http/Controllers/RatingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Rating;
 use App\Models\User;
+use App\Models\Ride;
 
 class RatingController extends Controller
 {
@@ -14,31 +15,33 @@ class RatingController extends Controller
             'rating' => 'required|integer|between:1,5',
             'comment' => 'nullable|string|max:255', 
         ]);
-
+    
         $user = $request->user();
-
+    
         if (!$user->isPassenger() && !$user->isDriver()) {
             return response()->json(['error' => 'Unauthorized to rate'], 403);
         }
-
+    
         $ratedUser = User::findOrFail($ratedUserId);
-
+    
+        // Check if there is a ride associated with the users
+        $ride = Ride::where(function ($query) use ($user, $ratedUser) {
+            $query->where('driver_id', $user->id)->where('passenger_id', $ratedUser->id);
+        })->orWhere(function ($query) use ($user, $ratedUser) {
+            $query->where('driver_id', $ratedUser->id)->where('passenger_id', $user->id);
+        })->first();
+    
+        if (!$ride) {
+            return response()->json(['error' => 'Unauthorized to rate. Users must have a associated ride.'], 403);
+        }
+    
         Rating::create([
             'user_id' => $user->id,
             'rated_user_id' => $ratedUser->id,
             'rating' => $request->input('rating'),
             'comment' => $request->input('comment'),
         ]);
-
+    
         return response()->json(['message' => 'Rating recorded successfully'], 200);
-    }
-
-    public function getAverageRating($userId)
-    {
-        $user = User::findOrFail($userId);
-
-        $averageRating = $user->getAverageRating();
-
-        return response()->json(['average_rating' => $averageRating]);
-    }
+    }    
 }
